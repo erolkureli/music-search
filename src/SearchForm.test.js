b5d1ca0c5820983e1e00054b26182241c0324d2a
Erol Kureli <>
2021-02-02 00:46:07 +0000

    test added

--- src/SearchForm.test.js
+++ src/SearchForm.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { render, waitForElementToBeRemoved, fireEvent, within } from '@testing-library/react';
 import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
@@ -16,7 +17,15 @@ describe('SearchForm', () => {
         const wrapper = shallow(<SearchForm />);
         expect(wrapper.exists()).toBe(true);
     });
-    
+
+    it('should display searchText', async () => {
+        const onSearchTypeChange = jest.fn();
+        const onSearchTextChange = jest.fn();
+
+        const container = render(<SearchForm searchText="erol" searchType="album" onSearchTypeChange={onSearchTypeChange} onSearchTextChange={onSearchTextChange} />);
+        expect(container.getByDisplayValue('erol')).toBeInTheDocument();
+    });
+
 });
 
 
